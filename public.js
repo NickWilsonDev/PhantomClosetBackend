d521cc4f793d1b01fa52014a3935ca93e50192da
rfpoulos <rfpoulos@outlook.com>
2018-05-01 15:14:00 -0400

    adds get request for sets, also documentation

--- public.js
+++ public.js
@@ -2,8 +2,9 @@ const express = require('express');
 const public = new express.Router();
 
 let { postTokens, addUser } = require('./authorization');
+let { getSets } = require('./card-data');
 
-
+public.get('/sets', getSets);
 public.post('/signin', postTokens);
 public.post('/createaccount', addUser);
 
