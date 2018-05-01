6c73fa4ba485e31fa5255547f1b3af1ea95c77dd
Nick Wilson <logikfx@gmail.com>
2018-05-01 14:55:54 -0400

    better db utillity script for populating all_cards table, 1300 cards right now

--- db_util.sql
+++ db_util.sql
@@ -15,9 +15,9 @@ CREATE DATABASE pc;
 
 CREATE TABLE users (
     ID SERIAL PRIMARY KEY,
-    username VARCHAR (100),
-    password VARCHAR (100),
-    role VARCHAR (50)
+    username VARCHAR (100) UNIQUE NOT NULL,
+    password VARCHAR (100) NOT NULL,
+    role VARCHAR (50) NOT NULL
 );
 
 
@@ -42,15 +42,36 @@ CREATE TABLE cards_in_queue (
 
 
 CREATE TABLE all_cards (
-    ID SERIAL PRIMARY KEY,
-    name VARCHAR (100) UNIQUE NOT NULL,
+    cardID SERIAL PRIMARY KEY,
+    name VARCHAR (256) NOT NULL,
     manaCost VARCHAR (100),
-    colors text[],
-    colorIdentity text[]
+    cmc VARCHAR(10),
+    colors text,
+    colorIdentity text,
+    "type" text,
+    types text,
+    subtypes text,
+    rarity VARCHAR(50),
+    "set" VARCHAR(20),
+    setName VARCHAR(50),
+    "text" text,
+    artist VARCHAR(100),
+    "number" VARCHAR(20),
+    "power" VARCHAR(20),
+    toughness VARCHAR(20),
+    layout VARCHAR(20),
+    multiverseid VARCHAR(30),
+    imageUrl VARCHAR(256),
+    printings text,
+    originalText text,
+    originalType text,
+    id VARCHAR(80)
 );
 
 /* insert statements */
 /* from json */
+\copy all_cards(name, manaCost, cmc, colors, colorIdentity, "type", types, subtypes, rarity, "set", setName, "text", artist, "number", "power", toughness, layout, multiverseid, imageUrl, printings, originalText, originalType, id) FROM './cards.csv' WITH (FORMAT csv);
+
 INSERT INTO users (username, password, role) VALUES
     ('nick', 'nick', 'admin'),
     ('rachel', 'rachel', 'admin'),
