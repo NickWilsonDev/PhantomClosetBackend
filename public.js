284bea83a9be4fdbabd772fb466c7d0fd4280212
rfpoulos <rfpoulos@outlook.com>
2018-05-02 10:08:31 -0400

    plugs in a server route

--- public.js
+++ public.js
@@ -2,10 +2,11 @@ const express = require('express');
 const public = new express.Router();
 
 let { postTokens, addUser } = require('./authorization');
-let { getSets, getCardByName } = require('./card-data');
+let { getSets, getCardsByName, getCardsByNameAutoComplete } = require('./card-data');
 
 public.get('/sets', getSets);
-public.get('/cards/:name', getCardByName);
+public.get('/cards/:name', getCardsByName);
+public.get('/autocomplete/:name', getCardsByNameAutoComplete);
 public.post('/signin', postTokens);
 public.post('/createaccount', addUser);
 
